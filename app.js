97179a35a54dc71f6c1ef30781a99e3fcb0ff851
ZakyZN <zakyzn1999@gmail.com>
2024-02-17 16:21:06 +0800

    add file upload

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const router = require('./routes');
 const log = require('./middlewares/logger')
 
+
 app.use(log);       
-app.use(router);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(router);
 app.use((req, res, next)=> {
+    res.status(404);
     res.send({
         status: "Gagal",
         message: 'Resource' +req.originalUrl+'Not Found'
--- routes.js
+++ routes.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
+//library untuk upload file
+const multer  = require('multer')
+const upload = multer({dest:"upload"})
+const fs = require('fs');
+const path = require('path');
 
-router.get("/", (req, res) => {
-  res.send({
-    status: "Berhasil",
-    message: "Selamat datang di Basic ExpressJS",
-  });
-});
 router.get("/", (req, res) => {
   res.send({
     status: "Berhasil",
@@ -20,8 +19,21 @@ router.get("/namasiswa/:id", (req, res) => {
   });
 });
 
-// router.post('/namasiswa', (req, res) => {
-//     res.json(res.body);
-// });
+//pakai postman atau insomnia
+router.post('/namasiswa/', (req, res) => {
+    res.json(req.body);
+});
+router.post('/reg/', upload.single('image'), (req, res) => {
+  const {name} = req.body;
+  const image = req.file;
+  if (image){
+    const target = path.join(__dirname, 'upload', image.originalname);
+    fs.renameSync(image.path, target)
+    res.json({
+      name,
+      image
+    });
+  }
+ });
 
 module.exports = router;
